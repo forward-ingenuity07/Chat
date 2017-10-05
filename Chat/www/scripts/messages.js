835362f8bf931c3c97eeb6b28ad3d27e274af8de
Chipili Kafwilo <forward.ingenuity07@gmail.com>
2017-10-05 18:11:00 +0200

    first commit

---
+++ Chat/www/scripts/messages.js
@@ -0,0 +1,30 @@
+﻿/// <reference path="angular.min.js" />
+(function () {
+    var App = angular.module("module", []);
+    App.controller("controller", function ($scope, $http, $interval) {
+
+        
+        $http({
+            method: 'GET',
+            url: 'http://forwardingenuity.com/messages.php'
+        })
+        .then(function (response) {
+            $scope.msg = response.data
+            window.localStorage.setItem('response1',response.data)
+
+        })
+        $interval(function () {
+            $http({
+                method: 'GET',
+                url: 'http://forwardingenuity.com/messages.php'
+            })
+        .then(function (response) {
+            window.localStorage.setItem('response2', response.data)
+            if (window.localStorage.getItem('response1').length != window.localStorage.getItem('response2').length) {
+                $scope.msg.push((response.data[(response.data).length -1]))
+                window.localStorage.setItem('response1', response.data);
+            }
+        })
+        },500)
+    })
+}())
